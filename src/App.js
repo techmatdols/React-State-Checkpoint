fca1dc433508b6fac79738867b0a8de06d824087
techmatdols <mudoh160@gmail.com>
2023-08-30 13:35:37 +0100

    react state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+ import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullName: 'John Doe',
+      bio: 'Frontend Developer',
+      imgSrc: 'https://placekitten.com/200/200', // Just a placeholder image
+      profession: 'Software Engineer'
+    },
+    show: false,
+    intervalId: null,
+    timeSinceMount: 0
+  };
+
+  toggleShow = () => {
+    this.setState(prevState => ({
+      show: !prevState.show
+    }));
+  };
+
+  componentDidMount() {
+    const intervalId = setInterval(() => {
+      this.setState(prevState => ({
+        timeSinceMount: prevState.timeSinceMount + 1
+      }));
+    }, 1000);
+
+    this.setState({ intervalId });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { show, timeSinceMount } = this.state;
+
+    return (
+      <div className="App">
+        <button onClick={this.toggleShow}>Toggle Profile</button>
+        {show && (
+          <div>
+            <h2>{fullName}</h2>
+            <p>{bio}</p>
+            <img src={imgSrc} alt="Profile" />
+            <p>{profession}</p>
+          </div>
+        )}
+        <p>Time since component mounted: {timeSinceMount} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
